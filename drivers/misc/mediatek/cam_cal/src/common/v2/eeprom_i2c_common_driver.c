c45e3666354c40037b4c6d99c8df74cb59eabab1
Matsvei Niaverau <matvej2002@gmail.com>
2023-01-13 11:31:02 +0100

    drivers: mediatek: Import Xiaomi cam_cal changes
    Change-Id: I10a9cc3c6fc6bb25b60aeb850d50ed1dc68806a8
    (cherry picked from commit c060e093c7e1e6fa7cd6dc2a09a58e41f0ee527b)

--- drivers/misc/mediatek/cam_cal/src/common/v2/eeprom_i2c_common_driver.c
+++ drivers/misc/mediatek/cam_cal/src/common/v2/eeprom_i2c_common_driver.c
@@ -1,8 +1,16 @@
-/* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2019 MediaTek Inc.
+ * Copyright (C) 2019 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
-
 #define PFX "CAM_CAL"
 #define pr_fmt(fmt) PFX "[%s] " fmt, __func__
 
@@ -45,7 +53,7 @@
 #define EEPROM_I2C_MSG_SIZE_READ 2
 
 #ifndef EEPROM_I2C_READ_MSG_LENGTH_MAX
-#define EEPROM_I2C_READ_MSG_LENGTH_MAX 255
+#define EEPROM_I2C_READ_MSG_LENGTH_MAX 1024
 #endif
 #ifndef EEPROM_I2C_WRITE_MSG_LENGTH_MAX
 #define EEPROM_I2C_WRITE_MSG_LENGTH_MAX 32
